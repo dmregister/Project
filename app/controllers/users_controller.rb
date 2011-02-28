a3123972ad0b96476995cde59f3861193f2ddf29
David Register <dmregister1@gmail.com>
2011-02-28 02:46:04 -0500

    updated user controller, added value ''

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the David's RoR Project"
       redirect_to @user
     else
-      @user.password
-      @user.password_confirmation
+      page[:password].value = ''
+      page[:password_confirmation].value = ''
       @title = "Sign Up"
       render 'new'
     end
